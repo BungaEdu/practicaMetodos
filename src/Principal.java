f43531d91a2d7f0a6097dbd2410397ba423d6c2c
BungaEdu <eduardoflores.9595@gmail.com>
2022-04-13 20:02:03 +0200

    casa 13/04

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,41 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] Arg) {
 
+        Serie objeto1 = new Serie();
 
+        Serie objeto2 = new Serie("","");
+
+        Serie objeto3 = new Serie("",10,'D',"","",0);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime los siguientes datos a rellenar: ");
+
+        System.out.print("Titulo: ");
+        String titulo = sc.nextLine();
+        objeto3.setTitulo(titulo);
+
+        System.out.print("Número de capitulos: ");
+        int numCapitulos = sc.nextInt();
+        objeto3.setNumCapitulos(numCapitulos);
+
+        System.out.print("Género: ");
+        char genero = sc.next().charAt(0);
+        objeto3.setGenero(genero);
+
+        System.out.print("Guionista: ");
+        String guionista = sc.nextLine();
+        objeto3.setGuionista(guionista);
+
+        System.out.print("Actores: ");
+        String actores = sc.nextLine();
+        objeto3.setActores(actores);
+
+        System.out.print("Precio: ");
+        float precio = sc.nextFloat();
+        objeto3.setPrecio(precio);
+
+        System.out.println(objeto3);
     }
 }
--- src/Serie.java
+++ src/Serie.java
@@ -1,5 +1,77 @@
 public class Serie {
-    String titulo;
-    int numCapitulos=10;
-    char genero;
+    private String titulo;
+    private int numCapitulos;
+    private char genero;
+    private String guionista;
+    private String actores; //En el set y get también hay que poner corchetes en el array
+    private float precio;
+
+    /****************************CONSTRUCTORES***************/
+    Serie() {
+    }
+
+    Serie (String titulo, String guionista){
+        this.titulo=titulo;
+        this.guionista=guionista;
+    }
+
+    Serie (String titulo,int numCapitulos,char genero,String guionista,String actores,float precio) {
+        this.titulo=titulo;
+        this.numCapitulos=numCapitulos;
+        this.genero=genero;
+        this.guionista=guionista;
+        this.actores=actores;
+        this.precio=precio;
+    }
+
+    /****************************SETTER*********************/
+    public void setTitulo (String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setNumCapitulos (int numCapitulos) {
+        this.numCapitulos = numCapitulos;
+    }
+
+    public void setGenero (char genero) {
+        this.genero = genero;
+    }
+
+    public void setGuionista (String guionista){
+        this.guionista=guionista;
+    }
+
+    public void setActores (String actores) {
+        this.actores=actores;
+    }
+
+    public void setPrecio (float precio) {
+        this.precio=precio;
+    }
+
+    /****************************GETTER*********************/
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getNumCapitulos() {
+        return numCapitulos;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public String getGuionista() {
+        return guionista;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
 }
